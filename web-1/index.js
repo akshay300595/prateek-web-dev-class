7fd05d72e87c119503ad8c66008d487c741d463a
akshaysaxena05326 <akshaykumarsaxena@Akshays-MacBook-Pro.local>
2026-01-27 16:08:57 +0530

    webpage created

--- web-1/index.js
+++ web-1/index.js
@@ -8,23 +8,34 @@ function btnClick(){
     // console.log(paraTagByClass[0])
     // console.log(paraTagByTagName[1])
     // paraTagByTagName[1].innerHTML = 'I am clicked'
+    if(document.getElementsByClassName('firstHeading').length > 0) return;
 
+    else{
+        let h1Tag = document.createElement('h1'); //<h1></h1>
+    h1Tag.classList.add('firstHeading');
 
-    let h1Tag = document.createElement('h1'); //<h1></h1>
     let mytext = document.createTextNode('Heading is here'); // Heading is here
     h1Tag.appendChild(mytext) // <h1>Heading is here</h1>
 
     let emptyContainer = document.createElement('div');
     emptyContainer.appendChild(h1Tag); //<div><h1>Heading is here</h1> </div>
-
     document.body.appendChild(emptyContainer)
-    
 
+
+
+    document.getElementsByClassName('firstHeading')[0].addEventListener('click', eventListenerFunction)
+    }
+    
 }   
 
 
 function dropdownChange(value){
     console.log(value)
+}
+
+
+function eventListenerFunction(){
+    console.log('listener is working')
 }
 
 
@@ -45,9 +56,10 @@ function mouseHover(){
 
 
 
-// Hoisting
+// Hoisting - done
 // Event Delegation
-// Event Listeners
+// Event Listeners - done
 // scoping
 
 // Async Programming
+// prototyping
